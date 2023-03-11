315bf00c3dccba5c5517ac3a2ec44b5385ca70a1
Travis Staton <hello@travisstaton.com>
2023-03-11 12:31:21 -0500

    init

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "raspberry-pi-nix example";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    raspberry-pi-nix.url = "github:tstat/raspberry-pi-nix";
+  };
+
+  outputs = { self, nixpkgs, raspberry-pi-nix }:
+    let
+      inherit (nixpkgs.lib) nixosSystem;
+      basic-config = { pkgs, lib, ... }: {
+        time.timeZone = "America/New_York";
+        users.users.root.initialPassword = "root";
+        networking = {
+          hostName = "basic-example";
+          useDHCP = false;
+          interfaces = { wlan0.useDHCP = true; };
+        };
+        environment.systemPackages = with pkgs; [ bluez bluez-tools ];
+        hardware = {
+          bluetooth.enable = true;
+          raspberry-pi = {
+            config = {
+              all = {
+                base-dt-params = {
+                  # enable autoprobing of bluetooth driver
+                  # https://github.com/raspberrypi/linux/blob/c8c99191e1419062ac8b668956d19e788865912a/arch/arm/boot/dts/overlays/README#L222-L224
+                  krnbt = {
+                    enable = true;
+                    value = "on";
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+
+    in {
+      nixosConfigurations = {
+        rpi-example = nixosSystem {
+          system = "aarch64-linux";
+          modules = [ raspberry-pi-nix.nixosModules.raspberry-pi basic-config ];
+        };
+      };
+    };
+}
